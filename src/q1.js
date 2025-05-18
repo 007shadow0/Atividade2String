f0272c9bf92eec6cdd97259879fb1c6c22e0e82b
Vinícius Ramalho <jvramalho2009@gmail.com>
2025-05-18 17:02:36 -0300

    Questão 1
    Adicionei os comentários sobre o código

--- src/q1.js
+++ src/q1.js
@@ -1,9 +1,9 @@
 // O typeof verifica e retorna o tipo da variável
-function verificarsestring(valor){ 
+function verificarsestring(valor){  
     // Irá definir um parametro
     return typeof valor === "string";
     // Aqui irá verificar se o valor fornecido ée um tipo string.
-    // Caso seja uma string, retorna "true" e caso não seja, retorna "false"
+    // Caso seja uma string, retorna "true" e caso não seja, retorna "false" 
 }
 console.log(verificarsestring("JavaScript")) //true
 console.log(verificarsestring(1,2,3)) //false
