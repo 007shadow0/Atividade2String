f75b0703404fef62b04f71f63b0edc6d5b1bff3f
Vinícius Ramalho <jvramalho2009@gmail.com>
2025-05-18 18:50:41 -0300

    atualização
    modifiquei o comentário sobre o código

--- src/q5.js
+++ src/q5.js
@@ -1,15 +1,14 @@
 function abreviar(nome){
     let separar= nome.split(' ');
-    //divide o string "nome" em um array usando o o metodo .split
-    //separa devido o .split('')
+    //.split(" ") divide o string "nome" em um array, utilizando o espaço para separar as palavras
     
-    if (separar.length>= 2){// verifica se o array tem dois elementos, no caso o nome e o sobrenome
+    if (separar.length>= 2){// verifica se o array tem dois elementos ou mais, no caso o nomeinicial e o sobrenome
 
-        let nomeprincipal= separar[0];// o primerio elemento do array, no caso o primeiro nome
+        let nomeinicial= separar[0];// o primerio elemento do array, no caso o primeiro nome
         
-        let sobrenome = separar[1][0];// pega o o elemento no indice 1
+        let sobrenome = separar[1][0];// pega o primeiro caracter do segundo nome, no caso o sobrenome
         
-        return `${nomeprincipal} ${sobrenome}.`;
+        return `${nomeinicial} ${sobrenome}.`;
         //retorna o primeiro nome, seguido da inicial do sobrenome e um ponto
     } else {
         return nome;// Aqui retornará caso seja apenas um nome, sem necessidade da abreviação
