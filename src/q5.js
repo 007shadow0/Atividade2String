bca8a429c6a211bd04310a6429554aa2941e65b5
Vinícius Ramalho <jvramalho2009@gmail.com>
2025-05-18 16:09:24 -0300

    Update q5.js

--- src/q5.js
+++ src/q5.js
@@ -1,17 +1,17 @@
 function abreviar(nome){
-    //
-    //
-    //
-    let separar= nome.split(' ');//
-    //
-    //
-    //
-    if (separar.length>= 2){//
-        let nomeprincipal= separar[0];//
-        let sobrenome = separar[1][0];//
-        return `${nomeprincipal} ${sobrenome}.`;//
+    let separar= nome.split(' ');
+    //divide o string "nome" em um array usando o o metodo .split
+    //separa devido o .split('')
+    if (separar.length>= 2){// verifica se o array tem dois elementos, no caso o nome e o sobrenome
+
+        let nomeprincipal= separar[0];// o primerio elemento do array, no caso o primeiro nome
+        
+        let sobrenome = separar[1][0];// pega o 
+        
+        return `${nomeprincipal} ${sobrenome}.`;
+        //retorna o primeiro nome, seguido da inicial do sobrenome e um ponto
     } else {
-        return nome;//
+        return nome;// Aqui retornará caso seja apenas um nome, sem necessidade da abreviação
     }
 }
 console.log(abreviar("Vinícius Ramalho"));// Vinícius R.
